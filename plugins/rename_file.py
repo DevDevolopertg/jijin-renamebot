b49e7c4a3472bcace45af2a820c9bb052ddb32d2
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-07-23 18:20:45 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -148,9 +148,10 @@ async def rename_doc(bot, message):
                 chat_id=message.chat.id,
                 document=new_file_name,
                 thumb=thumb_image_path,
-                parse_mode="md",
-                caption=script.CAPTION.format(file_name),
+                parse_mode="html",
+                # caption=script.CAPTION.format(file_name),
                 # reply_markup=reply_markup,
+                caption=(media.caption and media.caption.html) or file_name or " ",
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
